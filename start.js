9551cd8683a539852f15725ddf70bd87735dcf83
FoxFireYato <yatogamipromotions@gmail.com>
2018-03-22 03:16:44 +0000

    Stage

--- start.js
+++ start.js
@@ -9,9 +9,3 @@ process.on('unhandledRejection', err => {
 process.on('uncaughtException', err => {
     console.log(`Uncaught exception: \n${err.stack}`, 'error');
 });
-
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host, function () {
-    console.log('Listening on port %d', server_port);
-});
